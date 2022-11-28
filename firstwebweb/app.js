96da18e3222fbfcc5f2da5a3cc7cdc2f05499fe2
Jst Vex <heinhtetlulu14@gmail.com>
2022-11-29 01:48:39 +0630

    added tips for u

--- firstwebweb/app.js
+++ firstwebweb/app.js
@@ -1,2 +1,3 @@
 const h1 = document.querySelector('.h1')
 h1.innerHTML = "hi";
+// since u already know dom u shud start learning wither react or sth else i use react personally and u shud too it is a great and the most popular one
